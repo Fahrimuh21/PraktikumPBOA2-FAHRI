83aeabb1317c7f599011461f5b54ecebc24f568a
Muhammad Fahri <fahrimuh775@gmail.com>
2026-04-23 00:06:25 +0700

    ADD Pertemuan6

---
+++ Pertemuan6/UniversalInclusionMahasiswa/src/Mahasiswa.java
@@ -0,0 +1,35 @@
+//Nama : Muhammad Fahri
+//NIM : 24060124120037
+//Deskripsi : Membuat class Mahasiswa sebagai subclass dari Civitasakademika dengan atribut NIM dan dosen wali. Class ini memiliki method untuk mengatur dosen wali dan menampilkan data mahasiswa, termasuk NIM, nama, dan nama dosen wali. Method getNomor di override untuk mengembalikan NIM sebagai nomor identitas mahasiswa.
+
+public class Mahasiswa extends Civitasakademika {
+    private String NIM;
+    private Dosen dosenWali;
+
+    public Mahasiswa(String nama, String NIM, Dosen dosenWali) {
+        super(nama);
+        this.NIM = NIM;
+        this.dosenWali = dosenWali;
+    }
+
+    @Override
+    public String getNomor() {
+        return NIM;
+    }
+
+    public String getNIM() {
+        return NIM;
+    }
+
+    // i. set wali
+    public void setWali(Dosen d) {
+        this.dosenWali = d;
+    }
+
+    // j. tampil data mahasiswa
+    public void tampilDataMahasiswa() {
+        System.out.println("NIM   : " + NIM);
+        System.out.println("Nama  : " + nama);
+        System.out.println("Wali  : " + dosenWali.getNama());
+    }
+}
---
+++ Pertemuan6/UniversalInclusionMahasiswa/src/Seminar.java
@@ -0,0 +1,49 @@
+//Nama : Muhammad Fahri
+//NIM : 24060124120037
+//Deskripsi : Membuat class Seminar untuk mengelola peserta seminar yang terdiri dari berbagai jenis civitas akademika, seperti mahasiswa, dosen, dan tendik. Class ini memiliki atribut array untuk menyimpan peserta seminar dan metode untuk menghitung jumlah peserta, melakukan registrasi peserta, menampilkan daftar peserta, dan menghitung jumlah mahasiswa yang terdaftar dalam seminar. Met
+
+
+public class Seminar {
+    private Civitasakademika[] pesertas = new Civitasakademika[100];
+    private int banyakPeserta;
+
+    // b. konstruktor
+    public Seminar() {
+        banyakPeserta = 0;
+    }
+
+    // c. countPeserta
+    public int countPeserta() {
+        return banyakPeserta;
+    }
+
+    // d. registrasi
+    public void registrasi(Civitasakademika c) {
+        if (banyakPeserta < 100) {
+            pesertas[banyakPeserta] = c;
+            banyakPeserta++;
+        } else {
+            System.out.println("Kapasitas penuh!");
+        }
+    }
+
+    // g. tampil peserta
+    public void tampilPeserta() {
+        for (int i = 0; i < banyakPeserta; i++) {
+            System.out.println(
+                pesertas[i].getNomor() + " - " + pesertas[i].getNama()
+            );
+        }
+    }
+
+    // h. count mahasiswa
+    public int countMahasiswa() {
+        int jumlah = 0;
+        for (int i = 0; i < banyakPeserta; i++) {
+            if (pesertas[i] instanceof Mahasiswa) {
+                jumlah++;
+            }
+        }
+        return jumlah;
+    }
+}
