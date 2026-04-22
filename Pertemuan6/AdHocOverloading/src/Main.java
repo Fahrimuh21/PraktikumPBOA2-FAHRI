0cd79c99d5819708ef5cc6a6a0fe6e687c09b81c
Muhammad Fahri <fahrimuh775@gmail.com>
2026-04-23 00:14:05 +0700

    Tambah folder Pertemuan6 ke main

---
+++ Pertemuan6/AdHocOverloading/src/Main.java
@@ -0,0 +1,40 @@
+//Nama : Muhammad Fahri
+//NIM : 24060124120037
+//Deskripsi : membuat aplikasi untuk menguji konstruktor dan method overloading pada class Mahasiswa. Terdapat 3 konstruktor dan 3 method setProgramStudi yang berbeda parameter. Juga terdapat method tampil untuk menampilkan informasi mahasiswa.
+
+public class Main {
+    public static void main(String[] args) {
+
+        // c. Konstruktor tanpa parameter
+        Mahasiswa m1 = new Mahasiswa();
+        System.out.println("Objek m1 (default):");
+        m1.tampil();
+
+        // d. Konstruktor 3 parameter
+        Mahasiswa m2 = new Mahasiswa("120037", "Fahri", "Informatika");
+        System.out.println("Objek m2 (parameter):");
+        m2.tampil();
+
+        // e. Copy constructor (clone)
+        Mahasiswa m3 = new Mahasiswa(m2);
+        System.out.println("Objek m3 (clone dari m2):");
+        m3.tampil();
+
+        // b. Uji semua varian setProgramStudi
+
+        // varian 1 (tanpa parameter)
+        m1.setProgramStudi();
+        System.out.println("Set tanpa parameter:");
+        m1.tampil();
+
+        // varian 2 (String)
+        m1.setProgramStudi("Sistem Informasi");
+        System.out.println("Set dengan String:");
+        m1.tampil();
+
+        // varian 3 (objek)
+        m1.setProgramStudi(m2);
+        System.out.println("Set dengan objek Mahasiswa:");
+        m1.tampil();
+    }
+}
