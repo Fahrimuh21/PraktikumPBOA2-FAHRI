f1b8e81cfdcb87605fe565e7b55e78ecbefc1e51
Muhammad Fahri <fahrimuh775@gmail.com>
2026-04-23 00:09:31 +0700

    Clean up branch: Hanya menyisakan Pertemuan6

--- Pertemuan5/Latihan/src/Pajak.java
+++
@@ -1,9 +0,0 @@
-//Nama : Muhammad Fahri
-//NIM : 24060124120037
-//Deskripsi : Membuat interface Pajak untuk menghitung pajak yang harus dibayar oleh Pengusaha dan Pekerja
-
-public interface Pajak {
-
-    //Menghitung pajak yang harus dibayar
-    public double hitungPajak();    
-}
