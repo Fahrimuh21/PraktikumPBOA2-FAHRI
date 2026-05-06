4b29e5fb7358131056aefd8796d04741b5f8dd05
Muhammad Fahri <fahrimuh775@gmail.com>
2026-05-06 22:42:41 +0700

    Keep only Pertemuan8

--- Pertemuan7/GenerikPadaOperator/src/Anggora.java
+++
@@ -1,9 +0,0 @@
-//Nama : Muhammad Fahri
-//NIM : 24060124120037
-//Deskripsi : Membuat kelas Anggora yang merupakan turunan dari kelas Kucing, dengan metode tambahan untuk mengeluarkan suara khas Anggora.
-
-public class Anggora extends Kucing {
-    public Anggora(int bobot) {
-        super(bobot);
-    }
-}
--- Pertemuan7/GenerikPadaOperator/src/Persia.java
+++
@@ -1,9 +0,0 @@
-//Nama : Muhammad Fahri
-//NIM : 24060124120037
-//Deskripsi : Membuat kelas Persia yang merupakan turunan dari kelas Kucing, dengan metode tambahan untuk mengeluarkan suara khas Persia.
-
-public class Persia extends Kucing {
-    public Persia(int bobot) {
-        super(bobot);
-    }
-}
