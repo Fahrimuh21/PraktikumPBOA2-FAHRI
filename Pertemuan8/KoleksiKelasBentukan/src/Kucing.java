ae08ed806af55a87abe37d90a7db146a6ec366b6
Muhammad Fahri <fahrimuh775@gmail.com>
2026-05-06 22:52:36 +0700

    Restore files after hard reset

---
+++ Pertemuan8/KoleksiKelasBentukan/src/Kucing.java
@@ -0,0 +1,30 @@
+//Nama : Muhammad Fahri
+//NIM : 24060124120037
+//Deskripsi : Membuat kelas Kucing dengan atribut bobot, serta metode untuk mengakses bobot tersebut. Kelas ini akan menjadi kelas induk untuk kelas Anggora dan KembangTelon dalam konteks generik pada operator.
+
+public class Kucing extends Anabul {
+    protected int bobot;
+    private String nama;
+
+    public void Kucing() {
+        this.nama = "Default";
+        this.bobot = 0;
+    }
+
+    public Kucing(String nama, int bobot) {
+        this.nama = nama;
+        this.bobot = bobot;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public int getBobot() {
+        return bobot;
+    }
+}
---
+++ Pertemuan8/KoleksiKelasBentukan/src/Persia.java
@@ -0,0 +1,9 @@
+//Nama : Muhammad Fahri
+//NIM : 24060124120037
+//Deskripsi : Membuat kelas Persia yang merupakan turunan dari kelas Kucing, dengan metode tambahan untuk mengeluarkan suara khas Persia.
+
+public class Persia extends Kucing {
+    public Persia(String nama, int bobot) {
+        super(nama, bobot);
+    }
+}
